8464d998081f5700963c22eba2e2da5097d0325c
Shiielty <56679623+Shiielty@users.noreply.github.com>
2022-12-24 10:14:22 +0700

    feat(*): style the DOM

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ console.log('Hello, world!')
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
 const img = document.querySelector('img')
-const cityElem = document.querySelector('.name')
+const cityElem = document.querySelector('.city')
 const weatherElem = document.querySelector('.weather')
+const weatherDescElem = document.querySelector('.weather-desc')
 const tempElem = document.querySelector('.temp')
 const feelsElem = document.querySelector('.feels')
 const humidityElem = document.querySelector('.humidity')
+const minTempElem = document.querySelector(".min-temp")
+const maxTempElem = document.querySelector(".max-temp")
 
 
 const getCurrentWeather = async (city) => {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0eb089dd7b64c6aad23737c804ed8201`)
     const data = await response.json()
+    console.log(data);
 
     if (data.cod === 200) {
       return data
@@ -33,22 +37,28 @@ const getCurrentWeather = async (city) => {
 
 const Weather = (promiseData) => {
   const city = promiseData.name
-  const weather = promiseData.weather[0].description
+  const weather = promiseData.weather[0].main
   const weatherIcon = promiseData.weather[0].icon
+  const weatherDesc = promiseData.weather[0].description
   const temp = promiseData.main.temp
   const feelsLike = promiseData.main.feels_like
   const humidity = promiseData.main.humidity
+  const minTemp = promiseData.main.temp_min
+  const maxTemp = promiseData.main.temp_min
 
-  return { city, weather, weatherIcon, temp, feelsLike, humidity }
+  return { city, weather, weatherIcon, weatherDesc, temp, feelsLike, humidity, minTemp, maxTemp }
 }
 
 const updateDOM = (obj) => {
   img.src = `http://openweathermap.org/img/wn/${obj.weatherIcon}@2x.png`
-  cityElem.textContent = `City: ${obj.city}`
-  weatherElem.textContent = `Weather: ${obj.weather}`
-  tempElem.textContent = `Temperature: ${Math.round(obj.temp - 273)} °C`
-  feelsElem.textContent = `Feels Like: ${Math.round(obj.feelsLike - 273)} °C`
-  humidityElem.textContent = `Humidity: ${Math.round(obj.humidity)}%`
+  cityElem.textContent = obj.city
+  weatherElem.textContent = obj.weather
+  weatherDescElem.textContent = obj.weatherDesc
+  tempElem.textContent = `${Math.round(obj.temp - 273)} °C`
+  feelsElem.textContent = `${Math.round(obj.feelsLike - 273)} °C`
+  humidityElem.textContent = `${obj.humidity}%`
+  minTempElem.textContent = `${Math.round(obj.minTemp - 273)} °C`
+  maxTempElem.textContent = `${Math.round(obj.maxTemp - 273)} °C`
 }
 
 const newWeather = async() => {
